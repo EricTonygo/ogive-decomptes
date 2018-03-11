6cecbe04b629c8eb4b0decdc24e04c0be38e9741
EricTonygo <tonye.eric@gmail.com>
2018-03-11 14:31:29 +0100

    new changes of 11/03/2018

--- src/OGIVE/ProjectBundle/Form/DecompteTaskType.php
+++ src/OGIVE/ProjectBundle/Form/DecompteTaskType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class DecompteTaskType extends AbstractType {
 
@@ -15,24 +16,24 @@ class DecompteTaskType extends AbstractType {
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('nom', TextType::class, array('required' => false))
-                ->add('numero', null, array('required' => false))
-                ->add('unite', null, array('required' => false))
-                ->add('prixUnitaire', null, array('required' => false))
-                ->add('qtePrevueMarche', null, array('required' => false))
-                ->add('qtePrevueProjetExec', null, array('required' => false))
-                ->add('qteCumulMoisPrec', null, array('required' => false))
+                ->add('nom', HiddenType::class, array('required' => false))
+                ->add('numero', HiddenType::class, array('required' => false))
+                ->add('unite', HiddenType::class, array('required' => false))
+                ->add('prixUnitaire', HiddenType::class, array('required' => false))
+                ->add('qtePrevueMarche', HiddenType::class, array('required' => false))
+                ->add('qtePrevueProjetExec', HiddenType::class, array('required' => false))
+                ->add('qteCumulMoisPrec', HiddenType::class, array('required' => false))
                 ->add('qteMois', null, array('required' => false))
-                ->add('qteCumulMois', null, array('required' => false))
-                ->add('mtPrevueMarche', null, array('required' => false))
-                ->add('mtPrevueProjetExec', null, array('required' => false))
-                ->add('mtCumulMoisPrec', null, array('required' => false))
-                ->add('mtMois', null, array('required' => false))
-                ->add('mtCumulMois', null, array('required' => false))
-                ->add('pourcentRealisation', null, array('required' => false))
-                ->add('startDate', TextType::class, array('required' => false))
-                ->add('endDate', TextType::class, array('required' => false))
-                ->add('description', null, array('required' => false))
+                ->add('qteCumulMois', HiddenType::class, array('required' => false))
+                ->add('mtPrevueMarche', HiddenType::class, array('required' => false))
+                ->add('mtPrevueProjetExec', HiddenType::class, array('required' => false))
+                ->add('mtCumulMoisPrec', HiddenType::class, array('required' => false))
+                ->add('mtMois', HiddenType::class, array('required' => false))
+                ->add('mtCumulMois', HiddenType::class, array('required' => false))
+                ->add('pourcentRealisation', HiddenType::class, array('required' => false))
+                ->add('startDate', HiddenType::class, array('required' => false))
+                ->add('endDate', HiddenType::class, array('required' => false))
+                ->add('description', HiddenType::class, array('required' => false))
                 ->add('subDecompteTasks', CollectionType::class, array(
                     'entry_type' => SubDecompteTaskType::class,
                     'allow_add' => true,
